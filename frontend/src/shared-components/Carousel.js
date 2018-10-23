78929b2c9022c00697143dfdb74525aecff8e6d2
Uriel Kugelmass <urikugelmass@gmail.com>
2018-10-23 14:51:49 -0500

    Fixed styling on splash page

--- frontend/src/shared-components/Carousel.js
+++ frontend/src/shared-components/Carousel.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import {Charities} from './../img/Charities.jpg';
+import "./styles.css";
 const usMap = {
     height: '100%',
     width: '100%',
 }
 export default () => (
-  <Carousel autoPlay>
+  <Carousel showThumbs={false} showStatus={false}>
     <div style = {usMap}>
-      <iframe src="https://createaclickablemap.com/map.php?&id=74893&maplocation=false&online=true" height="100%"></iframe>
+      <iframe src="https://createaclickablemap.com/map.php?&id=74893&maplocation=false&online=true" height="100%" width="100%"></iframe>
       <p className="legend">States</p>
     </div>
     <div>
@@ -16,7 +16,7 @@ export default () => (
       <p className="legend">Charities</p>
     </div>
     <div>
-      <img className="d-block w-100" src={require('./../img/News.jpg')} alt="Third slide"/>
+      <img className="d-block w-100" src={require('./../img/News.jpeg')} alt="Third slide"/>
       <p className="legend">News</p>
     </div>
   </Carousel>
