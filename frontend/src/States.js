2bdfca42ef155fb55150836dcb9a94d712fd848c
Yoav Ilan <ilan.yoav@utexas.edu>
2018-10-24 15:30:55 -0500

    Created states model page with pagination

--- frontend/src/States.js
+++ frontend/src/States.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 import StatesCard from './shared-components/StatesCard';
+import {Grid, Row, Col} from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
+import './Paginate.css'
+
 
 class States extends Component{
+    constructor (props) {
+       super(props);
+
+       this.state = {data: [], total: 0}
+    }
+
+    getJSON(url) {
+        return fetch(url).then(response => {
+            return response.json();
+        });
+    }
+
+    componentWillMount() {
+        this.getJSON('https://api.relievepoverty.me/v1/states?page=1').then(response => {
+            this.setState(JSON.parse(JSON.stringify(response)))
+        });
+    }
+
+    handlePageClick = (data) =>{
+        let selected = data.selected + 1;
+        this.getJSON('https://api.relievepoverty.me/v1/states?page='+selected).then(response => {
+            this.setState(JSON.parse(JSON.stringify(response)))
+        });
+    }
+
     render(){
+        let pageSize = 12.0;
         return(
-            <StatesCard image={'http://www.theus50.com/images/state-flags/california-flag.jpg'} state={'California'} rank={1}/>
+            <div>
+                <div className='album py-5 bg-light listingPage'>
+                    <div className='row'>
+                        {this.state.data.map(obj =>
+                            <StatesCard image={obj.flag} state={obj.name} rank={obj.rank}/>
+                        )}
+                    </div>
+                </div>
+                <ReactPaginate className='pagination'
+                        previousLabel={"previous"}
+                           nextLabel={"next"}
+                           breakLabel={<a href="">...</a>}
+                           breakClassName={"break-me"}
+                           pageCount={Math.ceil(this.state.total / pageSize)}
+                           marginPagesDisplayed={2}
+                           pageRangeDisplayed={5}
+                           onPageChange={this.handlePageClick}
+                           containerClassName={"pagination"}
+                           subContainerClassName={"pages pagination"}
+                           activeClassName={"active"} />
+            </div>
         )
     }
 }
