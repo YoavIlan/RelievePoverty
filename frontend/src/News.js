0c01124b150eac593a63137e08a9271f75731545
Uriel Kugelmass <urikugelmass@gmail.com>
2018-10-25 15:04:21 -0500

    Added grid page for News with pagination

--- frontend/src/News.js
+++ frontend/src/News.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
 import NewsCard from './shared-components/NewsCard';
+import {Grid, Row, Col} from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
+import './Paginate.css'
+
 
 class News extends Component{
+    constructor (props) {
+       super(props);
+
+       this.state = {data: [], total: 0}
+    }
+
+    getJSON(url) {
+        return fetch(url).then(response => {
+            return response.json();
+        });
+    }
+
+    componentWillMount() {
+        this.getJSON('https://api.relievepoverty.me/v1/news?page=1').then(response => {
+            this.setState(JSON.parse(JSON.stringify(response)))
+        });
+    }
+
+    handlePageClick = (data) =>{
+        let selected = data.selected + 1;
+        this.getJSON('https://api.relievepoverty.me/v1/news?page='+selected).then(response => {
+            this.setState(JSON.parse(JSON.stringify(response)))
+        });
+    }
+
     render(){
+        let pageSize = 12.0;
         return(
-            <NewsCard title={'Yo!'} description={'Lorem imposum blah blah blah'} image={require('./img/albert.jpg')}/>
+            <>
+              <div className='album py-5 bg-light listingPage'>
+                <div class="container">
+                  <div className='row'>
+                    {this.state.data.map(obj =>
+                      <NewsCard image={obj.image} title={obj.title} description={obj.summary}/>
+                    )}
+                  </div>
+                </div>
+                <ReactPaginate className='pagination'
+                  previousLabel={"Previous"}
+                  nextLabel={"Next"}
+                  breakLabel={<a href="">...</a>}
+                  breakClassName={"break-me"}
+                  pageCount={Math.ceil(this.state.total / pageSize)}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={this.handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"} />
+              </div>
+            </>
         )
     }
 }
