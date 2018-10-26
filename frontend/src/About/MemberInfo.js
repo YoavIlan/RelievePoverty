ab0e21135582d8dbf6c0c298ecc0aaf35c9fa801
Albert Luu <aluu@utexas.edu>
2018-10-26 04:41:38 -0500

    added member info into a separate class

---
+++ frontend/src/About/MemberInfo.js
@@ -0,0 +1,70 @@
+export function getTeam() {
+  let members = {};
+  members["aluu"] = [
+    "Uriel Kugelmass",
+    0,
+    0,
+    0,
+    "Uriel is currently a Computer Science Senior at UT from Montevideo, Uruguay. " +
+      "Outside of school, you can find him playing soccer or reading books, but " +
+      "never taking long walks on the beach.",
+    "uri.jpg",
+    "Frontend Developer",
+  ];
+  members["Uriel Kugelmass"] = [
+    "Evan Weiss",
+    0,
+    0,
+    0,
+    "Evan is a Junior at UT studying Computer Science and was born in Dallas, TX. A " +
+      "fun fact about him is that he has been skydiving.He doesn't enjoy long walks on " +
+      "the beach because he gets bored easily.",
+    "evan.jpg",
+    "APIs, Backend, and Databases",
+  ];
+  members["Yoav Ilan"] = [
+    "Yoav Ilan",
+    0,
+    0,
+    0,
+    "Yoav is a Junior Computer Science student from Dallas, TX, " +
+      "and Israel prior to that. He only enjoys medium walks on the " +
+      "beach that are between 10 to 15 minutes inclusive.",
+    "yoav.jpg",
+    "Frontend Developer",
+  ];
+  members["Albert Luu"] = [
+    "Albert Luu",
+    0,
+    0,
+    0,
+    "Albert is a Junior Computer Science student from Arlington, Texas. Outside " +
+      "of school, he enjoys biking, gaming, and playing piano. Unfortunately, " +
+      "long walks on the beach are very tiring to him.",
+    "albert.jpg",
+    "Full-Stack Developer",
+  ];
+  members["cfrick16"] = [
+    "Colin Frick",
+    0,
+    0,
+    0,
+    "Colin is a Junior Computer Science student at UT from Houston Texas. " +
+      "He is interested in studying artificial intelligence and web development. " +
+      "He does not enjoy walking of any form.",
+    "colin.jpg",
+  ];
+  members["Daniel Chruscielski"] = [
+    "Daniel Chruscielski",
+    0,
+    0,
+    0,
+    "Daniel is a third year Computer Science student at UT.He enjoys making long " +
+      "walks on the beach shorter by finding the shortest possible path.In his spare " +
+      "time, he plays the drums, listens to jazz, and generally does what you would expect " +
+      "from someone who lives in Austin.",
+    "daniel.jpg",
+    "Full-Stack Developer",
+  ];
+  return members;
+}
