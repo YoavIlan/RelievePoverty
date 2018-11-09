3f460a20cc8faed0d376b846aefec3a50434d328
cfrick16 <cfrick16@gmail.com>
2018-11-09 12:32:25 -0600

    Added comments and cleaned up code in main.py

--- backend/main.py
+++ backend/main.py
@@ -92,8 +92,140 @@ class States(db.Model):
 
 db.create_all()
 
+# Gets a specific news article by its primary key
+@app.route("/v1/news/<id>", methods=['GET'])
+def getNewsById(id):
+    return flask.jsonify(News.serialize(News.query.get(id)))
+
+# Gets all news articles. Supports pagination, filtering, sorting, and searching
+@app.route("/v1/news", methods=['GET'])
+def getAllNews():
+    news = []
+    # Get all search queries
+    query = request.args.get('q')
+    # If search is empty then add all instances to news, else only add the articles that the search query matched
+    if query is None:
+        news = News.query.all()
+    else:
+        news = news_search(query)
+
+    # Searing for filters as arguments and filtering out those that don't mach the filter
+    # Year requires a special filter due to the DateTime type
+    if 'year' in request.args:
+        news = filter(lambda n: News.serialize(n)['published_date'].year == int(request.args['year']), news)
+    filters = ['state', 'author', 'source']
+    for fil in filters:
+        if(fil in request.args):
+            news = filter(lambda n: str(News.serialize(n)[fil]) == request.args[fil], news)
+
+    # Sort by the sort_by variable, reverse only if reverse is true
+    if "sort_by" in request.args:
+        news.sort(key=lambda x: News.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
+
+    total = len(news)
+    # If a page was requested, only return the instances that would be on that page
+    if 'page' in request.args:
+        result = list()
+        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
+        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
+            result.append(news[i-1])
+        return flask.jsonify({'data': [News.serialize(news) for news in result], 'total': total})
+    # If there were no pages were requested, then return all instances
+    return flask.jsonify({'data': [News.serialize(news) for news in news], 'total': total})
+
+# Gets a specific charity by its primary key
+@app.route("/v1/charities/<id>", methods=['GET'])
+def getCharityById(id):
+    return flask.jsonify(Charities.serialize(Charities.query.get(id)))
+
+# Gets all charities. Supports pagination, filters, sorting, and searching
+@app.route("/v1/charities", methods=['GET'])
+def getAllCharities():
+    charities = []
+    # Get all search query
+    query = request.args.get('q')
+    # If search is empty then add all instances to news, else only add the articles that the search query matched
+    if query is None:
+        charities = Charities.query.all()
+    else:
+        charities = charities_search(query)
+
+    # Filters out any instances using the given filters
+    filters = ['state', 'rating', 'affiliation','tax_classification', 'cause']
+    for fil in filters:
+        if(fil in request.args):
+            charities = filter(lambda n: str(Charities.serialize(n)[fil]) == request.args[fil], charities)
+
+    # Sorts the instances by the given variable in sort_by
+    if "sort_by" in request.args :
+        if(request.args.get('sort_by') in ["rating"]):
+            charities.sort(key=lambda x: float(Charities.serialize(x)[request.args.get('sort_by')]), reverse=(request.args.get('reverse')=='true'))
+        else:
+            charities.sort(key=lambda x: Charities.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
+
+    total = len(charities)
+    # If a page is provided than only return the instances that should appear on that page
+    if 'page' in request.args:
+        result = list()
+        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
+        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
+            result.append(charities[i-1])
+        return flask.jsonify({'data': [Charities.serialize(charity) for charity in result], 'total': total})
+    # If there is no page provided, return all the instances
+    return flask.jsonify({'data': [Charities.serialize(charity) for charity in charities], 'total': total})
+
+# Gets a specific state by its name
+@app.route("/v1/states/<state>", methods=['GET'])
+def getStateByName(state):
+    return flask.jsonify(States.serialize(States.query.filter_by(name=state)[0]))
+
+# Gets all states. Supports pagination, filtering, sorting, and searching
+@app.route("/v1/states", methods=['GET'])
+def getAllStates():
+    states = []
+    # Get all search queries
+    query = request.args.get('q')
+    # If there is no search query add all state instances, else add only the instances that match the search query
+    if query is None:
+        states = States.query.all()
+    else:
+        states = state_search(query)
+
+    # Filter by any filter provided
+    filters = ['median_income', 'below_poverty_rate', 'child_poverty_rate']
+    for fil in filters:
+        if (fil + "_low") in request.args:
+            states = filter(lambda n: (float(States.serialize(n)[fil]) >= float(request.args[fil + '_low'])) and (float(States.serialize(n)[fil]) < float(request.args[fil+'_high'])), states)
+
+    # Sort by the variables given in the sort_by argument
+    if "sort_by" in request.args :
+        # Converts these variables to floats so it can compare.
+        if(request.args.get('sort_by') in ["below_poverty_rate", "child_poverty_rate", "median_income"]):
+            states.sort(key=lambda x: float(States.serialize(x)[request.args.get('sort_by')]), reverse=(request.args.get('reverse')=='true'))
+        else:
+            states.sort(key=lambda x: States.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
+
+    total = len(states)
+    # If a page is requested return only the instnaces required by the page
+    if 'page' in request.args:
+        result = list()
+        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
+        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
+            result.append(states[i-1])
+        return flask.jsonify({'data': [States.serialize(state) for state in result], 'total': total})
+    # If no pages are requested return all the state instances
+    return flask.jsonify({'data': [States.serialize(state) for state in states], 'total': total})
 
 
+@app.route("/")
+def home():
+    return "Welcome to the RelievePoverty.me API!<br>You can find the documentation at <a href='https://documenter.getpostman.com/view/5460449/RWgjY1qy'>https://documenter.getpostman.com/view/5460449/RWgjY1qy</a>"
+
+"""
+Helper method used by all of the search functions
+Takes in an array where the array holds the point values where the
+indexes are the primary key that holds that point value
+"""
 def getIDsByPoints(points):
     ids = []
     all_zero = False
@@ -104,142 +236,22 @@ def getIDsByPoints(points):
         ids.append(index)
     return ids
 
+# Helper method for news_search and charities_search
+# Adds val to the points array for the primary keys of the instances that were given
 def updatePoints(val, points, instances, adj=0):
     for f in instances:
         points[f.id - 1 + adj] += val
 
+# Helper method for state_search
+# Adds val to the points array for the primary keys of the instances that were given
 def updatePointsState(val, points, instances):
     for f in instances:
         points[f.rank - 1] += val
 
 
-
-# Gets a specific news article by its primary key
-
-
-@app.route("/v1/news/<id>", methods=['GET'])
-def getNewsById(id):
-    return flask.jsonify(News.serialize(News.query.get(id)))
-
-# Gets all news articles. Supports pagination and filtering by states.
-
-
-@app.route("/v1/news", methods=['GET'])
-def getAllNews():
-    news = []
-    query = request.args.get('q')
-    if query is None:
-        news = News.query.all()
-    else:
-        news = news_search(query)
-    if 'state' in request.args:
-        news = filter(lambda n: News.serialize(n)['state'] == request.args['state'], news)
-    if 'year' in request.args:
-        news = filter(lambda n: News.serialize(n)['published_date'].year == int(request.args['year']), news)
-    filters = ['state', 'author', 'source']
-    for fil in filters:
-        if(fil in request.args):
-            news = filter(lambda n: str(News.serialize(n)[fil]) == request.args[fil], news)
-
-    if "sort_by" in request.args:
-        news.sort(key=lambda x: News.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
-    total = len(news)
-
-    if 'page' in request.args:
-        result = list()
-        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
-        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
-            result.append(news[i-1])
-        return flask.jsonify({'data': [News.serialize(news) for news in result], 'total': total})
-    return flask.jsonify({'data': [News.serialize(news) for news in news], 'total': total})
-
-# Gets a specific charity by its primary key
-
-
-@app.route("/v1/charities/<id>", methods=['GET'])
-def getCharityById(id):
-    return flask.jsonify(Charities.serialize(Charities.query.get(id)))
-
-# Gets all charities. Supports pagination and filtering by states.
-
-
-@app.route("/v1/charities", methods=['GET'])
-def getAllCharities():
-    charities = []
-    query = request.args.get('q')
-    if query is None:
-        charities = Charities.query.all()
-    else:
-        charities = charities_search(query)
-
-    filters = ['state', 'rating', 'affiliation','tax_classification', 'cause']
-    for fil in filters:
-        if(fil in request.args):
-            charities = filter(lambda n: str(Charities.serialize(n)[fil]) == request.args[fil], charities)
-
-    if "sort_by" in request.args :
-        if(request.args.get('sort_by') in ["rating"]):
-            charities.sort(key=lambda x: float(Charities.serialize(x)[request.args.get('sort_by')]), reverse=(request.args.get('reverse')=='true'))
-        else:
-            charities.sort(key=lambda x: Charities.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
-
-    total = len(charities)
-
-    if 'page' in request.args:
-        result = list()
-        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
-        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
-            result.append(charities[i-1])
-        return flask.jsonify({'data': [Charities.serialize(charity) for charity in result], 'total': total})
-    return flask.jsonify({'data': [Charities.serialize(charity) for charity in charities], 'total': total})
-
-# Gets a specific state by its name
-
-
-@app.route("/v1/states/<state>", methods=['GET'])
-def getStateByName(state):
-    return flask.jsonify(States.serialize(States.query.filter_by(name=state)[0]))
-
-# Gets all states. Supports pagination.
-
-
-@app.route("/v1/states", methods=['GET'])
-def getAllStates():
-    states = []
-    query = request.args.get('q')
-    if query is None:
-        states = States.query.all()
-    else:
-        states = state_search(query)
-
-    filters = ['median_income', 'below_poverty_rate', 'child_poverty_rate']
-    for fil in filters:
-        if (fil + "_low") in request.args:
-            states = filter(lambda n: (float(States.serialize(n)[fil]) >= float(request.args[fil + '_low'])) and (float(States.serialize(n)[fil]) < float(request.args[fil+'_high'])), states)
-
-    if "sort_by" in request.args :
-        if(request.args.get('sort_by') in ["below_poverty_rate", "child_poverty_rate", "median_income"]):
-            states.sort(key=lambda x: float(States.serialize(x)[request.args.get('sort_by')]), reverse=(request.args.get('reverse')=='true'))
-        else:
-            states.sort(key=lambda x: States.serialize(x)[request.args.get('sort_by')], reverse=(request.args.get('reverse')=='true'))
-
-    total = len(states)
-    if 'page' in request.args:
-        result = list()
-        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
-        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
-            result.append(states[i-1])
-        return flask.jsonify({'data': [States.serialize(state) for state in result], 'total': total})
-    return flask.jsonify({'data': [States.serialize(state) for state in states], 'total': total})
-
-
-@app.route("/")
-def home():
-    return "Welcome to the RelievePoverty.me API!<br>You can find the documentation at <a href='https://documenter.getpostman.com/view/5460449/RWgjY1qy'>https://documenter.getpostman.com/view/5460449/RWgjY1qy</a>"
-
-
-
+# Search for state instances given a query
 def state_search(query):
+    # Turn the query into a list of searach strings and remove bad characters
     searches = query.split(" ")
     for i in range(len(searches)):
         searches[i] = sub("[\\ \" \' ;]", "", searches[i])
@@ -261,8 +273,9 @@ def state_search(query):
 
     return results
 
-
+# Search for charity instances given a query
 def charities_search(query):
+    # Turn the query into a list of searach strings and remove bad characters
     searches = query.split(" ")
     for i in range(len(searches)):
         searches[i] = sub("[\\ \" \' ;]", "", searches[i])
@@ -284,8 +297,9 @@ def charities_search(query):
 
     return results
 
-
+# Search for charity instances given a query
 def news_search(query):
+    # Turn the query into a list of searach strings and remove bad characters
     searches = query.split(" ")
     for i in range(len(searches)):
         searches[i] = sub("[\\ \" \' ;]", "", searches[i])
@@ -313,6 +327,7 @@ def news_search(query):
 
 
 
+# Returns a json with three inner jsons one for each category
 @app.route("/v1", methods=['GET'])
 def all_search():
     if(request.args.get('q') is not None):
@@ -320,6 +335,7 @@ def all_search():
     else:
         return "No Query entered"
 
+    # Searches each category and saves results
     states = state_search(query)
     charities = charities_search(query)
     news = news_search(query)
@@ -357,54 +373,7 @@ def all_search():
     return flask.jsonify({'states': states_json, 'charities': charities_json,
         'news': news_json})
 
-    searches = query.split(" ")
-    for i in range(len(searches)):
-        searches[i] = sub("[\\ \" \' ;]", "", searches[i])
 
-
-    # Ranking Relevances by Points
-    news_adj = len(States.query.all())
-    c_adj = news_adj + len(News.query.all())
-    points = [0] * (c_adj + len(Charities.query.all()))
-    for s in searches:
-        if s is not "" :
-            x = "%" + s + "%"
-            updatePointsState(200, points, States.query.filter(States.name.like(x)).all())
-            updatePointsState(99, points, States.query.filter(States.counties.like(x)).all())
-            updatePoints(200, points, News.query.filter(News.title.like(x)).all(), news_adj)
-            updatePoints(99, points, News.query.filter(News.author.like(x)).all(), news_adj)
-            updatePoints(98, points, News.query.filter(News.state.like(x)).all(), news_adj)
-            updatePoints(97, points, News.query.filter(News.source.like(x)).all(), news_adj)
-            updatePoints(49, points, News.query.filter(News.summary.like(x)).all(), news_adj)
-            updatePoints(200, points, Charities.query.filter(Charities.name.like(x)).all(), c_adj)
-            updatePoints(99, points, Charities.query.filter(Charities.state.like(x)).all(), c_adj)
-
-
-    # Turning list of points with index=id into sorted list of ids
-    ids = getIDsByPoints(points)
-
-    # Grabbing instances by id in order
-    results = []
-    for i in ids:
-        if(i < news_adj):
-            results.append(States.serialize(States.query.get(i + 1)))
-        elif(i < c_adj):
-            results.append(News.serialize(News.query.get(i - news_adj + 1)))
-        else :
-            results.append(Charities.serialize(Charities.query.get(i-c_adj + 1)))
-
-
-    total = len(results)
-
-    if 'page' in request.args:
-        result = list()
-        i = (int(request.args['page']) - 1) * INSTANCES_PER_PAGE
-        for i in range(i + 1, min(i + INSTANCES_PER_PAGE, total) + 1):
-            result.append(results[i - 1])
-        return flask.jsonify({'data': result, 'total': total})
-
-
-    return flask.jsonify({'data': results, 'total': total})
 
 
 
