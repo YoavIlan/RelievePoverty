324f33ce1c4796279705f3535b39955c2db2ca5a
Daniel Chruscielski <daniel.chruscielski@utexas.edu>
2018-11-12 14:12:32 -0600

    Added filters to news and updated API functions

--- frontend/src/News.js
+++ frontend/src/News.js
@@ -4,14 +4,32 @@ import ReactPaginate from 'react-paginate';
 import fetch from 'node-fetch';
 import './Paginate.css'
 import Jumbotron from './shared-components/Jumbotron';
+import NativeSelects from './shared-components/Dropdown'
 
 
 class News extends Component{
     constructor (props) {
        super(props);
 
-       this.state = {data: [], total: 0, api: "https://api.relievepoverty.me/v1/news?page=", page: 1, query: ""}
+       this.state = {
+         data: [],
+         total: 0,
+         api: "https://api.relievepoverty.me/v1/news?page=",
+         page: 1,
+         query: "",
+         sort: "sort_by=state",
+         reverse: "",
+         filters: {}
+       }
     }
+
+    sorts = [
+      "title",
+      "source",
+      "state",
+      "author",
+      "published_date"
+    ]
 
     getJSON(url) {
         return fetch(url).then(response => {
@@ -25,74 +43,131 @@ class News extends Component{
         });
     }
 
+    handleFilterAuthor = (filter_value) => {
+        let str = "author=" + filter_value;
+        this.state.page = 1;
+        this.state.filters['author'] = str;
+        this.accessAPI();
+    }
+
+    handleFilterSource = (filter_value) => {
+        let str = "source=" + filter_value;
+        this.state.page = 1;
+        this.state.filters['source'] = str;
+        this.accessAPI();
+    }
+
+    handleFilterState = (filter_value) => {
+        let str = "state=" + filter_value;
+        this.state.page = 1;
+        this.state.filters['state'] = str;
+        this.accessAPI();
+    }
+
+    handleFilterYear = (filter_value) => {
+        let str = "year=" + filter_value;
+        this.state.page = 1;
+        this.state.filters['year'] = str;
+        this.accessAPI();
+    }
+
     handleSearch = (data) => {
         data.preventDefault();
-        this.state.query = data.target[0].value;
+        this.state.query = "q=" + data.target[0].value;
         this.state.page = 1;
-        if(this.state.query === ""){
-            this.getJSON(this.state.api + this.state.page).then(response => {
-                this.setState(JSON.parse(JSON.stringify(response)))
-            });
-        }
-        else{
-            this.getJSON(this.state.api + this.state.page + "&q=" + this.state.query).then(response => {
-                this.setState(JSON.parse(JSON.stringify(response)))
-            });
-        }
+        this.accessAPI();
+    }
+
+    handleReverse = (reverse) => {
+        this.state.page = 1;
+      if(reverse == "Descending")
+        this.state.reverse = "reverse=true"
+      else
+        this.state.reverse = ""
+
+      this.accessAPI();
+    }
+
+    handleSort = (sort_by) => {
+        this.state.page = 1;
+        let str = "sort_by=" + sort_by.replace(/ /g, '_');
+        this.state.sort = str;
+        this.accessAPI();
+    }
+
+    reset = () => {
+      this.setState({
+        data: [],
+        total: 0,
+        api: "https://api.relievepoverty.me/v1/news?page=",
+        page: 1,
+        query: "",
+        sort: "sort_by=state",
+        reverse: "",
+        filters: {},
+        reset: true
+      });
     }
 
     handlePageClick = (data) =>{
         this.state.page = data.selected + 1;
-        if(this.state.query === ""){
-            this.getJSON(this.state.api + this.state.page).then(response => {
-                this.setState(JSON.parse(JSON.stringify(response)))
-            });
-        }
-        else{
-            this.getJSON(this.state.api + this.state.page + "&q=" + this.state.query).then(response => {
-                this.setState(JSON.parse(JSON.stringify(response)))
-            });
-        }
+        this.accessAPI()
+    }
+
+    accessAPI = () => {
+      let args = [this.state.sort, this.state.reverse, this.state.query].concat(this.state.filters);
+      let api = this.state.api + this.state.page + "&";
+      for(let i = 0; i < args.length; i++){
+        if(args[i] != "")
+          api += args[i] + "&"
+      }
+      for(let i = 0; i < Object.keys(this.state.filters).length; i++){
+        api += this.state.filters[Object.keys(this.state.filters)[i]] + "&";
+      }
+      api = api.substring(0, api.length-1);
+      this.getJSON(api).then(response => {
+          this.setState(JSON.parse(JSON.stringify(response)))
+      })
     }
 
     render(){
         let pageSize = 12.0;
-        return(
+        let result =(
             <>
               <Jumbotron title={"Read Articles About Poverty in the US"} description={"Our news sources include The New York Times, CNN and others."} search={this.handleSearch} modelName={"news"} filters={[]} prompt={"blah"}/>
-                  <div className="container-fluid fss">
-                      <div className="row fss-bar">
-                    <div className="col-md-4 d-flex flex-column">
-
-                    </div>
-                    <div className="col-md-4 d-flex flex-column">
-
-                    </div>
-                    <div className="col-md-4 d-flex flex-column">
-                    {prompt &&
-                    <form onSubmit={this.handleSearch}>
-                        <input className="search-bar" type="text" placeholder="Search" />
-                        <input id="search-submit" type="submit" value="Submit" />
-                    </form>
-                    }
-                    </div>
-                    <div className="col-md-4 d-flex flex-column">
-
-                    </div>
-                    <div className="col-md-4 d-flex flex-column">
-
-                    </div>
-                    <div className="col-md-4 d-flex flex-column"></div>
-                    <div className="col-md-4 d-flex flex-column">
-
-                    </div>
-                    <div className="col-md-4 d-flex flex-column">
-                    {prompt &&
-                    <button onClick={this.reset}>Reset</button>
-                    }
-                    </div>
-                  </div>
-                  </div>
+              <div className="container-fluid fss">
+                  <div className="row fss-bar">
+                <div className="col-md-4 d-flex flex-column">
+                {prompt &&
+                <NativeSelects reset={this.state.reset} data={["Associated Press", "ABC News", "CNN", "The New York Times", "The Huffington Post"]} prompt={"Filter by Source"} onChange={this.handleFilterSource}></NativeSelects>
+                }
+                </div>
+                <div className="col-md-4 d-flex flex-column">
+                {prompt &&
+                <NativeSelects reset={this.state.reset} data={["Ascending", "Descending"]} prompt={"Sort Order"} onChange={this.handleReverse}></NativeSelects>
+                }
+                </div>
+                <div className="col-md-4 d-flex flex-column">
+                {prompt &&
+                <form onSubmit={this.handleSearch}>
+                    <input className="search-bar" type="text" placeholder="Search" />
+                    <input id="search-submit" type="submit" value="Submit" />
+                </form>
+                }
+                </div>
+                <div className="col-md-4 d-flex flex-column">
+                {prompt &&
+                <NativeSelects reset={this.state.reset} data={this.sorts} prompt={"Sort By"} onChange={this.handleSort}></NativeSelects>
+                }
+                </div>
+                <div className="col-md-4 d-flex flex-column"></div>
+                <div className="col-md-4 d-flex flex-column">
+                {prompt &&
+                <button onClick={this.reset}>Reset</button>
+                }
+                </div>
+              </div>
+              </div>
 
               <div className='album py-5 bg-light listingPage'>
                 <div className="container">
@@ -117,6 +192,11 @@ class News extends Component{
               </div>
             </>
         )
+        if(this.state.reset){
+          this.accessAPI();
+        }
+        this.state.reset = false;
+        return result;
     }
 }
 
