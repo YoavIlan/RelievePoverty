d658a56773a3d4f0bf7a97f7ddc5258d47bceef2
Uriel Kugelmass <urikugelmass@gmail.com>
2018-11-13 12:55:58 -0600

    Prettified News filters, search and sorts

--- frontend/src/News.js
+++ frontend/src/News.js
@@ -2,34 +2,35 @@ import React, { Component } from 'react';
 import NewsCard from './shared-components/NewsCard';
 import ReactPaginate from 'react-paginate';
 import fetch from 'node-fetch';
-import './Paginate.css'
+import './Paginate.css';
 import Jumbotron from './shared-components/Jumbotron';
-import NativeSelects from './shared-components/Dropdown'
-import './shared-components/styles.css'
+import NativeSelects from './shared-components/Dropdown';
+import './shared-components/styles.css';
+import './shared-components/Dropdown.css';
 
 
-class News extends Component{
-    constructor (props) {
-       super(props);
+class News extends Component {
+    constructor(props) {
+        super(props);
 
-       this.state = {
-         data: [],
-         total: 0,
-         api: "https://api.relievepoverty.me/v1/news?page=",
-         page: 1,
-         query: "",
-         sort: "sort_by=state",
-         reverse: "",
-         filters: {}
-       }
+        this.state = {
+            data: [],
+            total: 0,
+            api: "https://api.relievepoverty.me/v1/news?page=",
+            page: 1,
+            query: "",
+            sort: "sort_by=state",
+            reverse: "",
+            filters: {}
+        }
     }
 
     sorts = [
-      "title",
-      "source",
-      "state",
-      "author",
-      "published_date"
+        "title",
+        "source",
+        "state",
+        "author",
+        "published_date"
     ]
 
     getJSON(url) {
@@ -81,12 +82,12 @@ class News extends Component{
 
     handleReverse = (reverse) => {
         this.state.page = 1;
-      if(reverse == "Descending")
-        this.state.reverse = "reverse=true"
-      else
-        this.state.reverse = ""
+        if (reverse == "Descending")
+            this.state.reverse = "reverse=true"
+        else
+            this.state.reverse = ""
 
-      this.accessAPI();
+        this.accessAPI();
     }
 
     handleSort = (sort_by) => {
@@ -97,104 +98,105 @@ class News extends Component{
     }
 
     reset = () => {
-      this.setState({
-        data: [],
-        total: 0,
-        api: "https://api.relievepoverty.me/v1/news?page=",
-        page: 1,
-        query: "",
-        sort: "sort_by=state",
-        reverse: "",
-        filters: {},
-        reset: true
-      });
+        this.setState({
+            data: [],
+            total: 0,
+            api: "https://api.relievepoverty.me/v1/news?page=",
+            page: 1,
+            query: "",
+            sort: "sort_by=state",
+            reverse: "",
+            filters: {},
+            reset: true
+        });
     }
 
-    handlePageClick = (data) =>{
+    handlePageClick = (data) => {
         this.state.page = data.selected + 1;
         this.accessAPI()
     }
 
     accessAPI = () => {
-      let args = [this.state.sort, this.state.reverse, this.state.query].concat(this.state.filters);
-      let api = this.state.api + this.state.page + "&";
-      for(let i = 0; i < args.length; i++){
-        if(args[i] != "")
-          api += args[i] + "&"
-      }
-      for(let i = 0; i < Object.keys(this.state.filters).length; i++){
-        api += this.state.filters[Object.keys(this.state.filters)[i]] + "&";
-      }
-      api = api.substring(0, api.length-1);
-      this.getJSON(api).then(response => {
-          this.setState(JSON.parse(JSON.stringify(response)))
-      })
+        let args = [this.state.sort, this.state.reverse, this.state.query].concat(this.state.filters);
+        let api = this.state.api + this.state.page + "&";
+        for (let i = 0; i < args.length; i++) {
+            if (args[i] != "")
+                api += args[i] + "&"
+        }
+        for (let i = 0; i < Object.keys(this.state.filters).length; i++) {
+            api += this.state.filters[Object.keys(this.state.filters)[i]] + "&";
+        }
+        api = api.substring(0, api.length - 1);
+        this.getJSON(api).then(response => {
+            this.setState(JSON.parse(JSON.stringify(response)))
+        })
     }
 
-    render(){
+    render() {
         let pageSize = 12.0;
-        let result =(
+        let result = (
             <>
-              <Jumbotron title={"Read Articles About Poverty in the US"} description={"Our news sources include The New York Times, CNN and others."} search={this.handleSearch} modelName={"news"} filters={[]} prompt={"blah"}/>
-              <div className="container-fluid fss">
-                  <div className="row fss-bar">
-                <div className="col-md-4 d-flex flex-column">
-                {prompt &&
-                <NativeSelects reset={this.state.reset} data={["Associated Press", "ABC News", "CNN", "The New York Times", "The Huffington Post"]} prompt={"Filter by Source"} onChange={this.handleFilterSource}></NativeSelects>
-                }
+                <Jumbotron title={"Read Articles About Poverty in the US"} description={"Our news sources include The New York Times, CNN and others."} search={this.handleSearch} modelName={"news"} filters={[]} prompt={"blah"} />
+                <div className="container-fluid fss">
+                    <div className="row fss-bar">
+                        <div className="col-md-4 d-flex flex-column">
+                            {prompt &&
+                                <NativeSelects reset={this.state.reset} data={this.sorts} prompt={"Sort By"} onChange={this.handleSort}></NativeSelects>
+                            }
+                        </div>
+                        <div className="col-md-4 d-flex flex-column">
+                            {prompt &&
+                                <NativeSelects reset={this.state.reset} data={["Ascending", "Descending"]} prompt={"Sort Order"} onChange={this.handleReverse}></NativeSelects>
+                            }
+                        </div>
+                        <div className="d-flex flex-column search-form">
+                            {prompt &&
+                                <form onSubmit={this.handleSearch}>
+                                    <input className="search-bar" type="text" placeholder="Search" />
+                                    <input id="search-submit" type="submit" value="Submit" />
+                                </form>
+                            }
+                        </div>
+                    </div>
+                    <div className="row fss-bar">
+                        <div className="col-md-4 d-flex flex-column">
+                            {prompt &&
+                                <NativeSelects reset={this.state.reset} data={["Associated Press", "ABC News", "CNN", "The New York Times", "The Huffington Post"]} prompt={"Filter by Source"} onChange={this.handleFilterSource}></NativeSelects>
+                            }
+                        </div>
+                        <div className="col-md-3 d-flex flex-column" id="reset">
+                            {prompt &&
+                                <button className="reset-button" onClick={this.reset}>Reset</button>
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-4 d-flex flex-column">
-                {prompt &&
-                <NativeSelects reset={this.state.reset} data={["Ascending", "Descending"]} prompt={"Sort Order"} onChange={this.handleReverse}></NativeSelects>
-                }
-                </div>
-                <div className="col-md-4 d-flex flex-column">
-                {prompt &&
-                <form onSubmit={this.handleSearch}>
-                    <input className="search-bar" type="text" placeholder="Search" />
-                    <input id="search-submit" type="submit" value="Submit" />
-                </form>
-                }
-                </div>
-                <div className="col-md-4 d-flex flex-column">
-                {prompt &&
-                <NativeSelects reset={this.state.reset} data={this.sorts} prompt={"Sort By"} onChange={this.handleSort}></NativeSelects>
-                }
-                </div>
-                <div className="col-md-4 d-flex flex-column"></div>
-                <div className="col-md-4 d-flex flex-column">
-                {prompt &&
-                <button className="reset-button" onClick={this.reset}>Reset</button>
-                }
-                </div>
-              </div>
-              </div>
 
-              <div className='album py-5 bg-light listingPage'>
-                <div className="container">
-                  <div className='row'>
-                    {this.state.data.map(obj =>
-                      <NewsCard image={obj.image} title={obj.title} source={obj.source} id={obj.id} published_date={obj.published_date} state={obj.state} author={obj.author} query={this.state.query}/>
-                    )}
-                  </div>
+                <div className='album py-5 bg-light listingPage'>
+                    <div className="container">
+                        <div className='row'>
+                            {this.state.data.map(obj =>
+                                <NewsCard image={obj.image} title={obj.title} source={obj.source} id={obj.id} published_date={obj.published_date} state={obj.state} author={obj.author} query={this.state.query} />
+                            )}
+                        </div>
+                    </div>
+                    <ReactPaginate className='pagination'
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        breakLabel={<a href="#">...</a>}
+                        breakClassName={"break-me"}
+                        pageCount={Math.ceil(this.state.total / pageSize)}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={"pagination"}
+                        subContainerClassName={"pages pagination"}
+                        activeClassName={"active"} />
                 </div>
-                <ReactPaginate className='pagination'
-                  previousLabel={"Previous"}
-                  nextLabel={"Next"}
-                  breakLabel={<a href="#">...</a>}
-                  breakClassName={"break-me"}
-                  pageCount={Math.ceil(this.state.total / pageSize)}
-                  marginPagesDisplayed={2}
-                  pageRangeDisplayed={5}
-                  onPageChange={this.handlePageClick}
-                  containerClassName={"pagination"}
-                  subContainerClassName={"pages pagination"}
-                  activeClassName={"active"} />
-              </div>
             </>
         )
-        if(this.state.reset){
-          this.accessAPI();
+        if (this.state.reset) {
+            this.accessAPI();
         }
         this.state.reset = false;
         return result;
